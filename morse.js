d751b06ceecf86462e482786a20871e6486930f3
Cam Pedersen <diffference@gmail.com>
2012-06-19 22:06:12 -0400

    Implement initial dichotomic search and translate half of tree. LOL

--- morse.js
+++ morse.js
@@ -62,6 +62,119 @@ var map = {
   ' ': '.......'
 };
 
+var tree = {
+  '.': {
+    stop: 'E',
+    '.': {
+      stop: 'I',
+      '.': {
+        stop: 'S',
+        '.': {
+          stop: 'H',
+          '.': {
+            stop: '5'
+          },
+          '-': {
+            stop: '4'
+          }
+        },
+        '-': {
+          stop: 'V',
+          '.': {
+            stop: 'Ś'
+          },
+          '-': {
+            stop: '3'
+          }
+        }
+      },
+      '-': {
+        stop: 'U',
+        '.': {
+          stop: 'F',
+          // note lack of -
+          '.': {
+            stop: 'É'
+          }
+        },
+        '_': {
+          stop: 'Ü',
+          '.': {
+            stop: 'Ð',
+            '.': {
+              stop: '?'
+            },
+            '-': {
+              stop: '_'
+            }
+          },
+          '-': {
+            stop: '2'
+          }
+        }
+      }
+    },
+    '-': {
+      stop: 'A',
+      '.': {
+        stop: 'R',
+        '.': {
+          stop: 'L',
+          // note lack of .
+          '-': {
+            stop: 'È',
+            '.': {
+              stop: '"'
+            }
+          }
+        },
+        '-': {
+          stop: 'Ä',
+          // note lack of -
+          '.': {
+            stop: '+',
+            // note lack of .
+            '-': {
+              stop: '.'
+            }
+          }
+        }
+      },
+      '-': {
+        stop: 'W',
+        '.': {
+          stop: 'P',
+          // '.': {},
+          // TODO can't replicate character -_-
+          '-': {
+            stop: 'À',
+            // note lack of -
+            '.': {
+              stop: '@'
+            }
+          }
+        },
+        '-': {
+          stop: 'J',
+          '.': {
+            stop: 'Ĵ'
+          },
+          '-': {
+            stop: '1',
+            // note lack of -
+            '.': {
+              stop: "'"
+            }
+          }
+        }
+      }
+    }
+  },
+  '-': {
+
+  }
+};
+
 function encodeString (str) {
   var ret = str.split('');
   for (var i in ret) {
@@ -93,4 +206,14 @@ function decodeCharacterByMap (char) {
     }
   }
   return ' ';
+}
+
+function decodeCharacterByDichotomy (char) {
+  var sub = char.split('');
+  var cur;
+  var hold;
+  while (cur = sub.pop()) {
+    sub = sub[cur];
+  }
+  return sub.stop;
 }
